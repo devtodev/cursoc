d1615c1f116a83824c95e9da31de68a5137125af
CarlosMiguens <cmiguens@gmail.com>
2016-03-05 13:00:50 -0300

    primerCommit

---
+++ HolaMundo/src/HolaMundo.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : test1.c
+ Author      : Curso de C
+ Version     :
+ Description : Hola Mundo!
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hola nuevo mundo"); /* esto es un comentario */
+	return EXIT_SUCCESS;
+}
